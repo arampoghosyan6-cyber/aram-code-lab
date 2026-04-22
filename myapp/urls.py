ad997924482a4745b0afbb6869fd90031c5213b2
arampoghosyan6-cyber <aram.poghosyan6@edu.ysu.am>
2026-04-23 01:47:00 +0400

    Fix: Import create_temp_admin in urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import create_temp_admin  # <--- ԱՅՍ ՏՈՂԸ ՊԱԿԱՍՈՒՄ ԷՐ
 
 urlpatterns = [
     path('', views.home, name='home'),
